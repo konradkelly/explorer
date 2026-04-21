646c11369f3cce3c22b4fd73b2f817253b231b82
konradkelly <konradky@gmail.com>
2026-04-20 18:23:17 -0700

    Add unit tests for islands divided by water and mountain barriers

--- src/ExplorerSearchTest.java
+++ src/ExplorerSearchTest.java
@@ -50,4 +50,66 @@ public class ExplorerSearchTest {
         };
         assertEquals(9, ExplorerSearch.reachableArea(island));
     }
+
+    @Test
+    public void testSurroundedByObstacles() {
+        int[][] island = {
+            {1, 2, 1},
+            {3, 0, 2},
+            {1, 3, 1},
+        };
+        assertEquals(1, ExplorerSearch.reachableArea(island));
+    }
+
+
+    @Test
+    public void testIslandDividedByWater() {
+        int[][] island = {
+            {0, 1, 2, 1, 1},
+            {1, 1, 2, 1, 1},
+            {1, 1, 2, 1, 1},
+            {1, 1, 2, 1, 1},
+            {1, 1, 2, 1, 1},
+        };
+        assertEquals(10, ExplorerSearch.reachableArea(island));
+    }
+
+    @Test
+    public void testIslandDividedByMountains() {
+        int[][] island = {
+            {0, 1, 3, 1, 1},
+            {1, 1, 3, 1, 1},
+            {1, 1, 3, 1, 1},
+            {1, 1, 3, 1, 1},
+            {1, 1, 3, 1, 1},
+        };
+        assertEquals(10, ExplorerSearch.reachableArea(island));
+    }
+
+
+    @Test
+    public void testIslandDividedByMountainAndWater() {
+        int[][] island = {
+            {0, 1, 2, 1, 1},
+            {1, 1, 3, 1, 1},
+            {1, 1, 2, 1, 1},
+            {1, 1, 3, 1, 1},
+            {1, 1, 2, 1, 1},
+        };
+        assertEquals(10, ExplorerSearch.reachableArea(island));
+    }
+
+
+    @Test
+    public void testReachableArea_someUnreachable() {
+        int[][] island = {
+            {1, 1, 1, 3, 1, 1},
+            {3, 2, 3, 1, 3, 1},
+            {1, 1, 1, 1, 3, 3},
+            {3, 1, 2, 1, 0, 1},
+            {1, 1, 1, 2, 1, 1},
+        };
+        int actual = ExplorerSearch.reachableArea(island);
+        assertEquals(14, actual);
+    }
 }
