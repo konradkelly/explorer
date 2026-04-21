7561b20b8ea1be4299faf3ce26731a8b6e36992b
konradkelly <konradky@gmail.com>
2026-04-20 18:19:50 -0700

    Add unit tests for basic island layouts and boundary conditions

--- src/ExplorerSearchTest.java
+++ src/ExplorerSearchTest.java
@@ -2,19 +2,52 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ExplorerSearchTest {
+
+
     @Test
-    public void testReachableArea_someUnreachable() {
-        int[][] island = {
-            {1,1,1,3,1,1},
-            {3,2,3,1,3,1},
-            {1,1,1,1,3,3},
-            {3,1,2,1,0,1},
-            {1,1,1,2,1,1},
-        };
-        int actual = ExplorerSearch.reachableArea(island);
-        assertEquals(14, actual);
+    public void testSingleCell() {
+        int[][] island = {{0}};
+        assertEquals(1, ExplorerSearch.reachableArea(island));
     }
 
-    // Add more tests here!
-    // Come up with varied cases
+
+    @Test
+    public void testSingleRow() {
+        int[][] island = {{1, 1, 0, 1, 1}};
+        assertEquals(5, ExplorerSearch.reachableArea(island));
+    }
+
+
+    @Test
+    public void testSingleColumn() {
+        int[][] island = {
+            {1},
+            {1},
+            {0},
+            {1},
+            {1},
+        };
+        assertEquals(5, ExplorerSearch.reachableArea(island));
+    }
+
+
+    @Test
+    public void testExplorerOnEdge() {
+        int[][] island = {
+            {1, 0, 1},
+            {1, 1, 1},
+        };
+        assertEquals(6, ExplorerSearch.reachableArea(island));
+    }
+
+
+    @Test
+    public void testFullyOpen() {
+        int[][] island = {
+            {1, 1, 1},
+            {1, 0, 1},
+            {1, 1, 1},
+        };
+        assertEquals(9, ExplorerSearch.reachableArea(island));
+    }
 }
